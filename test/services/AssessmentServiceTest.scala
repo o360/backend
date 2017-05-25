0051dfd5d54cc3059d9f928c23b5a973c0436156
Egor Ilchenko <ilchenko_ei@bw-sw.com>
2017-05-25 15:26:03 +0700

    SOP-165 Unit test for assessment submiting

--- test/services/AssessmentServiceTest.scala
+++ test/services/AssessmentServiceTest.scala
@@ -5,13 +5,15 @@ import java.sql.Timestamp
 import models.assessment.{Answer, Assessment}
 import models.dao.{AnswerDao, EventDao, GroupDao, ProjectRelationDao}
 import models.event.Event
+import models.form.Form
 import models.project.Relation
 import models.user.UserShort
 import models.{ListWithTotal, NamedEntity}
 import org.mockito.ArgumentMatchers.{any, eq => eqTo}
 import org.mockito.Mockito._
+import org.scalacheck.Gen
 import testutils.fixture._
-import utils.errors.{ApplicationError, ConflictError, NotFoundError}
+import utils.errors.{ApplicationError, BadRequestError, ConflictError, NotFoundError}
 import utils.listmeta.ListMeta
 
 import scalaz.Scalaz.ToEitherOps
@@ -214,6 +216,255 @@ class AssessmentServiceTest
       result.swap.toOption.get mustBe a[ConflictError]
     }
 
-    // TODO Other test cases
+    "return error if selfvoting" in {
+      val fixture = getFixture
+
+      val user = UserFixture.user
+      val event = Events(0)
+      val projectId = 3
+      val userGroupsIds = Seq(1L, 2, 3)
+      val formId = 1
+      val answer = Answer.Form(NamedEntity(formId), Set())
+      val assessment = Assessment(Some(UserShort.fromUser(user)), Seq(answer))
+
+      when(fixture.groupDao.findGroupIdsByUserId(user.id)).thenReturn(toFuture(userGroupsIds))
+      when(fixture.eventDao.getList(
+        optId = eqTo(Some(event.id)),
+        optStatus = any[Option[Event.Status]],
+        optProjectId = eqTo(Some(projectId)),
+        optNotificationFrom = any[Option[Timestamp]],
+        optNotificationTo = any[Option[Timestamp]],
+        optFormId = any[Option[Long]],
+        optGroupFromIds = eqTo(Some(userGroupsIds)),
+        optEndFrom = any[Option[Timestamp]],
+        optEndTimeTo = any[Option[Timestamp]]
+      )(any[ListMeta]))
+        .thenReturn(toFuture(ListWithTotal(1, Seq(event))))
+
+      when(fixture.answerDao.getAnswer(event.id, projectId, user.id, Some(user.id), formId))
+        .thenReturn(toFuture(None))
+
+      val result = wait(fixture.service.submit(event.id, projectId, assessment)(user).run)
+
+      result mustBe 'left
+      result.swap.toOption.get mustBe a[BadRequestError]
+    }
+
+    "return error if can't validate form" in {
+      val baseForm = Form(1, "", Seq(), Form.Kind.Freezed)
+      val invalidFormsWithAnswers = Seq(
+        // Duplicate answers
+        baseForm ->
+          Answer.Form(NamedEntity(1), Set(Answer.Element(2, Some(""), None), Answer.Element(2, Some(""), None))),
+        // Missed required answer
+        baseForm.copy(elements = Seq(Form.Element(1, Form.ElementKind.TextArea, "", required = true, Nil))) ->
+          Answer.Form(NamedEntity(1), Set()),
+        // Values elements contains text
+        baseForm.copy(elements = Seq(Form.Element(1, Form.ElementKind.Select, "", required = true, Nil))) ->
+          Answer.Form(NamedEntity(1), Set(Answer.Element(1, Some(""), None))),
+        // Text element contains values answer
+        baseForm.copy(elements = Seq(Form.Element(1, Form.ElementKind.TextArea, "", required = true, Nil))) ->
+          Answer.Form(NamedEntity(1), Set(Answer.Element(1, None, Some(Seq(1))))),
+        // Values answer contains unknown element
+        baseForm.copy(elements = Seq(Form.Element(1, Form.ElementKind.Select, "", required = true, Seq(Form.ElementValue(3, ""))))) ->
+          Answer.Form(NamedEntity(1), Set(Answer.Element(1, None, Some(Seq(1))))),
+        // Text answer is missed
+        baseForm.copy(elements = Seq(Form.Element(1, Form.ElementKind.TextArea, "", required = true, Nil))) ->
+          Answer.Form(NamedEntity(1), Set(Answer.Element(1, None, None))),
+        // Answer for unknown element id
+        baseForm.copy(elements = Seq(Form.Element(1, Form.ElementKind.TextArea, "", required = false, Nil))) ->
+          Answer.Form(NamedEntity(1), Set(Answer.Element(2, Some(""), None)))
+      )
+
+      forAll(Gen.oneOf(invalidFormsWithAnswers)) { case (form, answer) =>
+        val fixture = getFixture
+
+        val user = UserFixture.user
+        val event = Events(0)
+        val projectId = 3
+        val userGroupsIds = Seq(1L, 2, 3)
+        val formId = answer.form.id
+        val assessment = Assessment(None, Seq(answer))
+
+        when(fixture.groupDao.findGroupIdsByUserId(user.id)).thenReturn(toFuture(userGroupsIds))
+        when(fixture.eventDao.getList(
+          optId = eqTo(Some(event.id)),
+          optStatus = any[Option[Event.Status]],
+          optProjectId = eqTo(Some(projectId)),
+          optNotificationFrom = any[Option[Timestamp]],
+          optNotificationTo = any[Option[Timestamp]],
+          optFormId = any[Option[Long]],
+          optGroupFromIds = eqTo(Some(userGroupsIds)),
+          optEndFrom = any[Option[Timestamp]],
+          optEndTimeTo = any[Option[Timestamp]]
+        )(any[ListMeta]))
+          .thenReturn(toFuture(ListWithTotal(1, Seq(event))))
+
+        when(fixture.answerDao.getAnswer(event.id, projectId, user.id, None, formId))
+          .thenReturn(toFuture(None))
+
+        when(fixture.formService.getById(formId))
+          .thenReturn(EitherT.eitherT(toFuture(\/-(form): ApplicationError \/ Form)))
+
+        val result = wait(fixture.service.submit(event.id, projectId, assessment)(user).run)
+
+        result mustBe 'left
+      }
+    }
+
+    "return error if there is no available relations for user" in {
+      val fixture = getFixture
+
+      val user = UserFixture.user
+      val event = Events(0)
+      val projectId = 3
+      val userGroupsIds = Seq(1L, 2, 3)
+      val form = Form(1, "", Seq(Form.Element(1, Form.ElementKind.TextField, "", true, Nil)), Form.Kind.Freezed)
+      val answer = Answer.Form(NamedEntity(form.id), Set(Answer.Element(1, Some("text"), None)))
+      val assessment = Assessment(None, Seq(answer))
+
+      when(fixture.groupDao.findGroupIdsByUserId(user.id)).thenReturn(toFuture(userGroupsIds))
+      when(fixture.eventDao.getList(
+        optId = eqTo(Some(event.id)),
+        optStatus = any[Option[Event.Status]],
+        optProjectId = eqTo(Some(projectId)),
+        optNotificationFrom = any[Option[Timestamp]],
+        optNotificationTo = any[Option[Timestamp]],
+        optFormId = any[Option[Long]],
+        optGroupFromIds = eqTo(Some(userGroupsIds)),
+        optEndFrom = any[Option[Timestamp]],
+        optEndTimeTo = any[Option[Timestamp]]
+      )(any[ListMeta]))
+        .thenReturn(toFuture(ListWithTotal(1, Seq(event))))
+
+      when(fixture.answerDao.getAnswer(event.id, projectId, user.id, None, form.id))
+        .thenReturn(toFuture(None))
+
+      when(fixture.formService.getById(form.id))
+        .thenReturn(EitherT.eitherT(toFuture(\/-(form): ApplicationError \/ Form)))
+
+      when(fixture.relationDao.getList(
+        optId = any[Option[Long]],
+        optProjectId = eqTo(Some(projectId)),
+        optKind = eqTo(Some(Relation.Kind.Survey))
+      )(any[ListMeta])).thenReturn(toFuture(ListWithTotal[Relation](0, Nil)))
+
+      val result = wait(fixture.service.submit(event.id, projectId, assessment)(user).run)
+
+      result mustBe 'left
+      result.swap.toOption.get mustBe a[ConflictError.Assessment.WrongParameters.type]
+    }
+
+    "return error if user not in from groups" in {
+      val fixture = getFixture
+
+      val user = UserFixture.user
+      val event = Events(0)
+      val projectId = 3
+      val userGroupsIds = Seq(1L, 2, 3)
+      val form = Form(1, "", Seq(Form.Element(1, Form.ElementKind.TextField, "", true, Nil)), Form.Kind.Freezed)
+      val answer = Answer.Form(NamedEntity(form.id), Set(Answer.Element(1, Some("text"), None)))
+      val assessment = Assessment(None, Seq(answer))
+      val relation = Relation(
+        id = 1,
+        project = NamedEntity(2),
+        groupFrom = NamedEntity(30),
+        groupTo = Some(NamedEntity(4)),
+        form = NamedEntity(5),
+        kind = Relation.Kind.Classic,
+        templates = Nil
+      )
+
+      when(fixture.groupDao.findGroupIdsByUserId(user.id)).thenReturn(toFuture(userGroupsIds))
+      when(fixture.eventDao.getList(
+        optId = eqTo(Some(event.id)),
+        optStatus = any[Option[Event.Status]],
+        optProjectId = eqTo(Some(projectId)),
+        optNotificationFrom = any[Option[Timestamp]],
+        optNotificationTo = any[Option[Timestamp]],
+        optFormId = any[Option[Long]],
+        optGroupFromIds = eqTo(Some(userGroupsIds)),
+        optEndFrom = any[Option[Timestamp]],
+        optEndTimeTo = any[Option[Timestamp]]
+      )(any[ListMeta]))
+        .thenReturn(toFuture(ListWithTotal(1, Seq(event))))
+
+      when(fixture.answerDao.getAnswer(event.id, projectId, user.id, None, form.id))
+        .thenReturn(toFuture(None))
+
+      when(fixture.formService.getById(form.id))
+        .thenReturn(EitherT.eitherT(toFuture(\/-(form): ApplicationError \/ Form)))
+
+      when(fixture.relationDao.getList(
+        optId = any[Option[Long]],
+        optProjectId = eqTo(Some(projectId)),
+        optKind = eqTo(Some(Relation.Kind.Survey))
+      )(any[ListMeta])).thenReturn(toFuture(ListWithTotal[Relation](1, Seq(relation))))
+
+      when(fixture.formService.getOrCreateFreezedForm(event.id, relation.form.id))
+        .thenReturn(EitherT.eitherT(toFuture(\/-(form): ApplicationError \/ Form)))
+
+      val result = wait(fixture.service.submit(event.id, projectId, assessment)(user).run)
+
+      result mustBe 'left
+      result.swap.toOption.get mustBe a[ConflictError.Assessment.WrongParameters.type]
+    }
+
+    "save answer in db" in {
+      val fixture = getFixture
+
+      val user = UserFixture.user
+      val event = Events(0)
+      val projectId = 3
+      val userGroupsIds = Seq(1L, 2, 3)
+      val form = Form(1, "", Seq(Form.Element(1, Form.ElementKind.TextField, "", true, Nil)), Form.Kind.Freezed)
+      val answer = Answer.Form(NamedEntity(form.id), Set(Answer.Element(1, Some("text"), None)))
+      val assessment = Assessment(None, Seq(answer))
+      val relation = Relation(
+        id = 1,
+        project = NamedEntity(2),
+        groupFrom = NamedEntity(3),
+        groupTo = None,
+        form = NamedEntity(5),
+        kind = Relation.Kind.Classic,
+        templates = Nil
+      )
+
+      when(fixture.groupDao.findGroupIdsByUserId(user.id)).thenReturn(toFuture(userGroupsIds))
+      when(fixture.eventDao.getList(
+        optId = eqTo(Some(event.id)),
+        optStatus = any[Option[Event.Status]],
+        optProjectId = eqTo(Some(projectId)),
+        optNotificationFrom = any[Option[Timestamp]],
+        optNotificationTo = any[Option[Timestamp]],
+        optFormId = any[Option[Long]],
+        optGroupFromIds = eqTo(Some(userGroupsIds)),
+        optEndFrom = any[Option[Timestamp]],
+        optEndTimeTo = any[Option[Timestamp]]
+      )(any[ListMeta]))
+        .thenReturn(toFuture(ListWithTotal(1, Seq(event))))
+
+      when(fixture.answerDao.getAnswer(event.id, projectId, user.id, None, form.id))
+        .thenReturn(toFuture(None))
+
+      when(fixture.formService.getById(form.id))
+        .thenReturn(EitherT.eitherT(toFuture(\/-(form): ApplicationError \/ Form)))
+
+      when(fixture.relationDao.getList(
+        optId = any[Option[Long]],
+        optProjectId = eqTo(Some(projectId)),
+        optKind = eqTo(Some(Relation.Kind.Survey))
+      )(any[ListMeta])).thenReturn(toFuture(ListWithTotal[Relation](1, Seq(relation))))
+
+      when(fixture.formService.getOrCreateFreezedForm(event.id, relation.form.id))
+        .thenReturn(EitherT.eitherT(toFuture(\/-(form): ApplicationError \/ Form)))
+
+      when(fixture.answerDao.saveAnswer(event.id, projectId, user.id, None, answer))
+        .thenReturn(toFuture(answer))
+
+      val result = wait(fixture.service.submit(event.id, projectId, assessment)(user).run)
+
+      result mustBe 'right
+    }
   }
 }
