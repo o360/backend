a4300608f5ab1a887c3425aa975c025860649b5d
Egor Ilchenko <ilchenko_ei@bw-sw.com>
2017-09-28 18:21:55 +0700

    Migration hotfix

--- conf/migrations/postgres/public/V2017.09.28.18.00__alter_relation_add_index.sql
+++ conf/migrations/postgres/public/V2017.09.28.18.00__alter_relation_add_index.sql
@@ -1,2 +1,2 @@
-CREATE UNIQUE INDEX relation_classic_unique ON relation (project_id, group_from_id, group_to_id, form_id) WHERE kind = 0
-CREATE UNIQUE INDEX relation_survey_unique ON relation (project_id, group_from_id, form_id) WHERE kind = 1
+CREATE UNIQUE INDEX relation_classic_unique ON relation (project_id, group_from_id, group_to_id, form_id) WHERE kind = 0;
+CREATE UNIQUE INDEX relation_survey_unique ON relation (project_id, group_from_id, form_id) WHERE kind = 1;
