c6bee626c54e087b266b0557b869df45171bd069
Maxim Skripnik <skripnik_ma@bw-sw.com>
2020-02-19 14:49:07 +0700

    refactor .collect to .flatten

--- app/silhouette/SilhouetteModule.scala
+++ app/silhouette/SilhouetteModule.scala
@@ -105,7 +105,7 @@ class SilhouetteModule extends AbstractModule {
     SocialProviderRegistry(
       Seq(
         googleProvider
-      ).collect { case Some(provider) => provider }
+      ).flatten
     )
 
   @Provides
